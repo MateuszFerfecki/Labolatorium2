001878e4a617dfccf4c5a2186b1a8a8b91efe31f
Mateusz Ferfecki <w69934@student.wsiz.edu.pl>
2024-12-05 14:49:55 +0100

    init

--- LABOLATORIUM_5/ZAD1.cs
+++ LABOLATORIUM_5/ZAD1.cs
@@ -46,5 +46,18 @@ class Program
      
         Shape square = new Square(5); //podajemy wartośc 
         Console.WriteLine("Pole kwadratu: " + square.CalculateArea());
+       
+        IVehicle car = new Car(200, 4);
+        car.Start();
+        Console.WriteLine($"Samochód ma maksymalnom prędkość: {car.MaxSpeed} km/h.");
+        (car as Car).Stop();
+        Console.WriteLine($"Samochód ma {((Car)car).NumberOfDoors} drzwi.\n");
+
+        
+        IVehicle bike = new Bike(25, true);
+        bike.Start();
+        Console.WriteLine($"Rower ma maksymalną prędkosć: {bike.MaxSpeed} km/h.");
+        (bike as Bike).RingBell();
+        (bike as Bike).Stop();
     }
 }
---
+++ LABOLATORIUM_5/ZAD2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+interface IVehicle
+{
+    int MaxSpeed { get; set; }
+    void Start();
+    void Stop();
+}
+
+class Car : IVehicle
+{  
+    public int MaxSpeed { get; set; }
+ 
+    public int NumberOfDoors { get; set; }
+
+    public Car(int maxSpeed, int numberOfDoors)
+    {
+        MaxSpeed = maxSpeed;
+        NumberOfDoors = numberOfDoors;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Samochód uruchomiony.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Samochód zatrzymany.");
+    }
+}
+
+class Bike : IVehicle
+{
+ 
+    public int MaxSpeed { get; set; }
+ 
+    public bool HasBell { get; set; }
+
+    public Bike(int maxSpeed, bool hasBell)
+    {
+        MaxSpeed = maxSpeed;
+        HasBell = hasBell;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Rower jedzie.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Rower zatrzymany.");
+    }
+
+    public void RingBell()
+    {
+        if (HasBell)
+            Console.WriteLine("Dzwonek roweru: Ding Ding");
+        else
+            Console.WriteLine("Rower nie ma dzwonka.");
+    }
+}
