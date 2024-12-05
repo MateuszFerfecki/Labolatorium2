6b34b3350279bc17f42d44c34c6dc2ea556b6c92
Mateusz Ferfecki <w69934@student.wsiz.edu.pl>
2024-12-05 14:07:28 +0100

    init

--- LABOLATORIUM_5/ZAD1.cs
+++ LABOLATORIUM_5/ZAD1.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Square : Shape
+{
+    private double sideLength;
+
+    public Square(double sideLength)
+    {
+        this.sideLength = sideLength;
+    }
+
+    public override double CalculateArea()
+    {
+        return sideLength * sideLength;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Shape circle = new Circle(2);//podajemy wartośc 
+        Console.WriteLine("Pole koła: " + circle.CalculateArea());
+
+     
+        Shape square = new Square(5); //podajemy wartośc 
+        Console.WriteLine("Pole kwadratu: " + square.CalculateArea());
+    }
+}
